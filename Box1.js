5bb35215571ed3921427a886a408485b8e308e82
Arjun56789 <78798815+Arjun56789@users.noreply.github.com>
2021-06-04 18:02:19 +0530

    Add files via upload

---
+++ Box1.js
@@ -0,0 +1,18 @@
+class Box1{
+    constructor(x, y, width, height){
+        var options = {
+            isStatic: false
+        }
+
+        this.body = Bodies.rectangle(x, y, width, height, options)
+        World.add(world, this.body);
+        this.width = width
+        this.height = height
+    }
+    display(){
+        var pos = this.body.position;
+        fill("blue");
+        rectMode(CENTER);
+        rect(pos.x, pos.y, this.width, this.height);
+    }
+}
